53019596b1966c7f92af186cd08ba1e54691bc16
strdak97 <str.dak97@protonmail.com>
2023-07-07 10:51:16 -0400

    refactoring echo server

--- FUND-120/4_ClientServerLab/echo/echoserver.c
+++ FUND-120/4_ClientServerLab/echo/echoserver.c
@@ -1,152 +1,146 @@
 #include <stdio.h>
+#include <getopt.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <getopt.h>
 #include <netdb.h>
-#include <sys/types.h>
+#include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 #define BUFSIZE 4096
-#define DEFAULT_PORT 8080
 
-/* Function prototypes */
-void error(const char *msg);
-void parseCommandLineArgs(int argc, char **argv, int *port, int *maxPending);
-void setupSocket(int *listenfd, int port);
-void acceptConnections(int listenfd);
+#define USAGE                                                                 \
+"usage:\n"                                                                    \
+"  echoserver [options]\n"                                                    \
+"options:\n"                                                                  \
+"  -h                  Show this help message\n"                              \
+"  -n                  Maximum pending connections\n"                         \
+"  -p                  Port (Default: 8080)\n"
+
+/* OPTIONS DESCRIPTOR ====================================================== */
+static struct option gLongOptions[] = {
+        {"port",        required_argument, NULL, 'p'},
+        {"maxnpending", required_argument, NULL, 'n'},
+        {"help",        no_argument,       NULL, 'h'},
+        {NULL, 0,                          NULL, 0}
+};
+
+/*
+ * error - wrapper for perror
+ */
+void error(char *msg) {
+  perror(msg);
+  exit(1);
+}
 
 int main(int argc, char **argv) {
-    int listenfd;        // Listening socket
-    int port;            // Port to listen on
-    int maxPending = 5;  // Maximum pending connections
-
-    parseCommandLineArgs(argc, argv, &port, &maxPending);
-
-    setupSocket(&listenfd, port);
-
-    acceptConnections(listenfd);
-
-    return 0;
-}
-
-/*
- * Prints an error message and exits the program.
- */
-void error(const char *msg) {
-    perror(msg);
-    exit(1);
-}
-
-/*
- * Parses and sets the command line arguments.
- */
-void parseCommandLineArgs(int argc, char **argv, int *port, int *maxPending) {
     int option_char;
-    const char *usage =
-        "usage:\n"
-        "  echoserver [options]\n"
-        "options:\n"
-        "  -h                  Show this help message\n"
-        "  -n                  Maximum pending connections\n"
-        "  -p                  Port (Default: 8080)\n";
+    int portno = 8080; /* port to listen on */
+    int maxnpending = 5;
 
-    struct option gLongOptions[] = {
-        {"port", required_argument, NULL, 'p'},
-        {"maxnpending", required_argument, NULL, 'n'},
-        {"help", no_argument, NULL, 'h'},
-        {NULL, 0, NULL, 0}
-    };
-
+    // Parse and set command line arguments
     while ((option_char = getopt_long(argc, argv, "p:n:h", gLongOptions, NULL)) != -1) {
         switch (option_char) {
-            case 'p':
-                *port = atoi(optarg);
+            case 'p': // listen-port
+                portno = atoi(optarg);
                 break;
-            case 'n':
-                *maxPending = atoi(optarg);
+            case 'n': // server
+                maxnpending = atoi(optarg);
                 break;
-            case 'h':
-                fprintf(stdout, "%s", usage);
+            case 'h': // help
+                fprintf(stdout, "%s", USAGE);
                 exit(0);
+                break;
             default:
-                fprintf(stderr, "%s", usage);
+                fprintf(stderr, "%s", USAGE);
                 exit(1);
         }
     }
 
-    if (*port < 1025 || *port > 65535) {
-        fprintf(stderr, "Invalid port number: %d\n", *port);
+    if ((portno < 1025) || (portno > 65535)) {
+        fprintf(stderr, "%s @ %d: invalid port number (%d)\n", __FILE__, __LINE__, portno);
         exit(1);
     }
 
-    if (*maxPending < 1) {
-        fprintf(stderr, "Invalid pending count: %d\n", *maxPending);
+    if (maxnpending < 1) {
+        fprintf(stderr, "%s @ %d: invalid pending count (%d)\n", __FILE__, __LINE__, maxnpending);
         exit(1);
     }
-}
 
-/*
- * Sets up the listening socket.
- */
-void setupSocket(int *listenfd, int port) {
-    int optval = 1;
-    struct sockaddr_in serveraddr;
-
-    *listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (*listenfd < 0)
+    /* socket: create a socket */
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd < 0) {
         error("ERROR opening socket");
+    }
 
-    setsockopt(*listenfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int));
+    /* setsockopt: Handy debugging trick that lets 
+    * us rerun the server immediately after we kill it; 
+    * otherwise we have to wait about 20 secs. 
+    * Eliminates "ERROR on binding: Address already in use" error. 
+    */
+    int optval = 1;
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, 
+        (const void *)&optval , sizeof(int));
 
-    bzero((char *)&serveraddr, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serveraddr.sin_port = htons((unsigned short)port);
+    /* build the server's internet address */
+    struct sockaddr_in serveraddr;
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET; /* we are using the Internet */
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); /* accept reqs to any IP addr */
+    serveraddr.sin_port = htons((unsigned short)portno); /* port to listen on */
 
-    if (bind(*listenfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
-        error("ERROR on binding");
+    /* bind: associate the listening socket with a port */
+    if (bind(listenfd, (struct sockaddr *) &serveraddr, 
+        sizeof(serveraddr)) < 0) {
+            error("ERROR on binding");
+    }
 
-    if (listen(*listenfd, 5) < 0)
+    /* listen: make it a listening socket ready to accept connection requests */
+    if (listen(listenfd, 5) < 0) {  /* allow 5 requests to queue up */ 
         error("ERROR on listen");
-}
-
-/*
- * Accepts incoming connections and handles each connection individually.
- */
-void acceptConnections(int listenfd) {
+    }
+    
+    /* main loop: wait for a connection request, echo input line, 
+    then close connection. */
     struct sockaddr_in clientaddr;
     socklen_t clientlen = sizeof(clientaddr);
-    struct hostent *hostp;
-    char buf[BUFSIZE];
-    int connfd;
-
     while (1) {
-        connfd = accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
-        if (connfd < 0)
+        /* accept: wait for a connection request */
+        int connfd = accept(listenfd, (struct sockaddr *) &clientaddr, &clientlen);
+        if (connfd < 0) {
             error("ERROR on accept");
+        } 
 
-        hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-        if (hostp == NULL)
+        /* gethostbyaddr: determine who sent the message */
+        struct hostent *hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
+            sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+        if (hostp == NULL) {
             error("ERROR on gethostbyaddr");
+        }
 
-        const char *hostaddrp = inet_ntoa(clientaddr.sin_addr);
-        if (hostaddrp == NULL)
-            error("ERROR on inet_ntoa");
+        char *hostaddrp = inet_ntoa(clientaddr.sin_addr);
+        if (hostaddrp == NULL) {
+            error("ERROR on inet_ntoa\n");
+        }
 
-        printf("Server established connection with %s (%s)\n", hostp->h_name, hostaddrp);
+        printf("Server established connection with %s (%s)\n", 
+        hostp->h_name, hostaddrp);
 
-        bzero(buf, BUFSIZE);
-        int n = read(connfd, buf, BUFSIZE);
-        if (n < 0)
+        /* read: read input string from the client */
+        char buf[BUFSIZE];
+        int bytesRead = read(connfd, buf, BUFSIZE);
+        if (bytesRead < 0) {
             error("ERROR reading from socket");
-        printf("Server received %d bytes: %s\n", n, buf);
+        }
+        printf("Server received %d bytes: %s", bytesRead, buf);
 
-        n = write(connfd, buf, strlen(buf));
-        if (n < 0)
+        /* write: echo the input string back to the client */
+        int bytesSent = write(connfd, buf, strlen(buf));
+        if (bytesSent < 0) {
             error("ERROR writing to socket");
+        }
 
         close(connfd);
     }
