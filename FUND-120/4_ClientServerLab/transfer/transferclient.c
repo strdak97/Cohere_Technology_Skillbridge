4555b427c1491a1b9e00cc28d1000b13c1551135
strdak97 <str.dak97@protonmail.com>
2023-07-07 13:32:20 -0400

    refactored transfer client and server, removed unecessary files

--- FUND-120/4_ClientServerLab/transfer/transferclient.c
+++ FUND-120/4_ClientServerLab/transfer/transferclient.c
@@ -29,28 +29,23 @@ static struct option gLongOptions[] = {
         {NULL, 0,                     NULL, 0}
 };
 
-/* 
+/*
  * error - wrapper for perror
  */
 void error(char *msg) {
-    fprintf(stderr, "%s\n", msg);
-    exit(0);
+  perror(msg);
+  exit(1);
 }
 
 /* Main ========================================================= */
 int main(int argc, char **argv) {
-    int sockfd;
-    struct sockaddr_in serveraddr;
-    struct hostent *server;
-    char buf[BUFSIZE];
-    /* */
     int option_char = 0;
     char *hostname = "localhost";
     unsigned short portno = 8080;
     char *filename = "foo.txt";
 
     // Parse and set command line arguments
-    while ((option_char = getopt_long(argc, argv, "s:p:m:h", gLongOptions, NULL)) != -1) {
+    while ((option_char = getopt_long(argc, argv, "s:p:o:h", gLongOptions, NULL)) != -1) {
         switch (option_char) {
             case 's': // server
                 hostname = optarg;
@@ -58,7 +53,7 @@ int main(int argc, char **argv) {
             case 'p': // listen-port
                 portno = atoi(optarg);
                 break;
-            case 'o': // message
+            case 'o': // filename
                 filename = optarg;
                 break;
             case 'h': // help
@@ -71,6 +66,11 @@ int main(int argc, char **argv) {
         }
     }
 
+    if (NULL == filename) {
+        fprintf(stderr, "%s @ %d: invalid filename\n", __FILE__, __LINE__);
+        exit(1);
+    }
+
     if ((portno < 1025) || (portno > 65535)) {
         fprintf(stderr, "%s @ %d: invalid port number (%d)\n", __FILE__, __LINE__, portno);
         exit(1);
@@ -80,56 +80,47 @@ int main(int argc, char **argv) {
         fprintf(stderr, "%s @ %d: invalid host name\n", __FILE__, __LINE__);
         exit(1);
     }
-
-    if (NULL == filename) {
-        fprintf(stderr, "%s @ %d: invalid filename\n", __FILE__, __LINE__);
-        exit(1);
-    }
-
-    /* socket Code Here */
-
+  
     /* socket: create the socket */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        error("ERROR opening socket\n");
+    } 
 
     /* gethostbyname: get the server's DNS entry */
-    server = gethostbyname(hostname);
+    struct hostent *server = gethostbyname(hostname);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host as %s\n", hostname);
         exit(0);
     }
 
     /* build the server's Internet address */
+    struct sockaddr_in serveraddr;
     bzero((char *) &serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
-
     bcopy((char *)server->h_addr, 
-    (char *)&serveraddr.sin_addr.s_addr, server->h_length);
-
+	  (char *)&serveraddr.sin_addr.s_addr, server->h_length);
     serveraddr.sin_port = htons(portno);
 
     /* connect: create a connection with the server */
-    if (connect(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) 
-        error("ERROR connecting");
+    if (connect(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) {
+        error("ERROR connecting to server\n");
+    }
 
-    /* test code */
-
+    // Open the file in binary mode for reading
     FILE *file = fopen(filename, "wb");
     if (file == NULL) {
-        error("ERROR opening file for writing");
+        error("ERROR opening file\n");
     }
 
     // Receive data from the server and write it to the file
-    int bytes_received;
-    while ((bytes_received = read(sockfd, buf, BUFSIZE)) > 0) {
-        if (fwrite(buf, sizeof(char), bytes_received, file) < bytes_received) {
-            error("ERROR writing to file");
+    int bytesRecv = 0;
+    char buf[BUFSIZE];
+    while ((bytesRecv = read(sockfd, buf, BUFSIZE)) > 0) {
+        if (fwrite(buf, sizeof(char), bytesRecv, file) < bytesRecv) {
+            error("ERROR writing to file\n");
         }
     }
-
-    // Close the file
-    fclose(file);
 
     close(sockfd);
     return 0;
--- FUND-120/4_ClientServerLab/transfer/transferserver.c
+++ FUND-120/4_ClientServerLab/transfer/transferserver.c
@@ -10,9 +10,6 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 
-#if 0
-#endif
-
 #define BUFSIZE 4096
 
 #define USAGE                                                                 \
@@ -40,16 +37,6 @@ void error(char *msg) {
 }
 
 int main(int argc, char **argv) {
-    int listenfd; /* listening socket */
-    int connfd; /* connection socket */
-    socklen_t clientlen; /* byte size of client's address */
-    struct sockaddr_in serveraddr; /* server's addr */
-    struct sockaddr_in clientaddr; /* client addr */
-    struct hostent *hostp; /* client host info */
-    char buf[BUFSIZE]; /* message buffer */
-    char *hostaddrp; /* dotted decimal host addr string */
-    int optval; /* flag value for setsockopt */
-    /* provided variables below */
     int option_char;
     int portno = 8080; /* port to listen on */
     char *filename = "bar.txt"; /* file to transfer */
@@ -73,33 +60,33 @@ int main(int argc, char **argv) {
         }
     }
 
-    if ((portno < 1025) || (portno > 65535)) {
-        fprintf(stderr, "%s @ %d: invalid port number (%d)\n", __FILE__, __LINE__, portno);
-        exit(1);
-    }
-
     if (NULL == filename) {
         fprintf(stderr, "%s @ %d: invalid filename\n", __FILE__, __LINE__);
         exit(1);
     }
 
-    /* Socket Code Here */
+    if ((portno < 1025) || (portno > 65535)) {
+        fprintf(stderr, "%s @ %d: invalid port number (%d)\n", __FILE__, __LINE__, portno);
+        exit(1);
+    }
 
     /* socket: create a socket */
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (listenfd < 0) 
-        error("ERROR opening socket");
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd < 0) {
+        error("ERROR opening socket\n");
+    }
 
     /* setsockopt: Handy debugging trick that lets 
     * us rerun the server immediately after we kill it; 
     * otherwise we have to wait about 20 secs. 
     * Eliminates "ERROR on binding: Address already in use" error. 
     */
-    optval = 1;
+    int optval = 1;
     setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, 
-            (const void *)&optval , sizeof(int));
+        (const void *)&optval , sizeof(int));
 
     /* build the server's internet address */
+    struct sockaddr_in serveraddr;
     bzero((char *) &serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET; /* we are using the Internet */
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); /* accept reqs to any IP addr */
@@ -107,44 +94,54 @@ int main(int argc, char **argv) {
 
     /* bind: associate the listening socket with a port */
     if (bind(listenfd, (struct sockaddr *) &serveraddr, 
-        sizeof(serveraddr)) < 0) 
-        error("ERROR on binding");
+        sizeof(serveraddr)) < 0) {
+            error("ERROR on binding\n");
+    }
 
     /* listen: make it a listening socket ready to accept connection requests */
-    if (listen(listenfd, 5) < 0) /* allow 5 requests to queue up */ 
-        error("ERROR on listen");
-
-    clientlen = sizeof(clientaddr);
+    if (listen(listenfd, 5) < 0) {  /* allow 5 requests to queue up */ 
+        error("ERROR on listen\n");
+    }
+    
+    /* wait for a connection request, transfer 
+       file, then close connection. */
+    struct sockaddr_in clientaddr;
+    socklen_t clientlen = sizeof(clientaddr);
 
     /* accept: wait for a connection request */
-    connfd = accept(listenfd, (struct sockaddr *) &clientaddr, &clientlen);
-    if (connfd < 0) 
-    error("ERROR on accept");
-    
-    /* gethostbyaddr: determine who sent the message */
-    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
-            sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-    if (hostp == NULL)
-      error("ERROR on gethostbyaddr");
-    hostaddrp = inet_ntoa(clientaddr.sin_addr);
-    if (hostaddrp == NULL)
-      error("ERROR on inet_ntoa\n");
-    printf("server established connection with %s (%s)\n", 
-    hostp->h_name, hostaddrp);
+    int connfd = accept(listenfd, (struct sockaddr *) &clientaddr, &clientlen);
+    if (connfd < 0) {
+        error("ERROR on accept\n");
+    } 
 
-  /* test code */
+    /* gethostbyaddr: determine who sent the message */
+    struct hostent *hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
+        sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+    if (hostp == NULL) {
+        error("ERROR on gethostbyaddr\n");
+    }
+
+    /* retrieves the IP address of client */
+    char *hostaddrp = inet_ntoa(clientaddr.sin_addr);
+    if (hostaddrp == NULL) {
+        error("ERROR on inet_ntoa\n");
+    }
+
+    printf("Server established connection with %s (%s)\n", 
+    hostp->h_name, hostaddrp);
 
     // Open the file in binary mode for reading
     FILE *file = fopen(filename, "rb");
     if (file == NULL) {
-        error("ERROR opening file");
+        error("ERROR opening file\n");
     }
 
     // Read the file and send its contents to the client
-    int bytes_read;
-    while ((bytes_read = fread(buf, sizeof(char), BUFSIZE, file)) > 0) {
-        if (write(connfd, buf, bytes_read) < 0) {
-            error("ERROR writing to socket");
+    int bytesRead = 0;
+    char buf[BUFSIZE];
+    while ((bytesRead = fread(buf, sizeof(char), BUFSIZE, file)) > 0) {
+        if (write(connfd, buf, bytesRead) < 0) {
+            error("ERROR writing to socket\n");
         }
     }
 
